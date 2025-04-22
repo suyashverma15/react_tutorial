50cf58322620f6f883b2ea3cfaa1daf7c220987f
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-04-22 23:35:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const heading = React.createElement(
     {id: "heading"},
     "Hello World from react!!");
 
-// nested element is created     
+// nested element is created  
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    React.createElement(
+    "div",
+    {id: "child"},
+    React.createElement("h1",{},"I am a h1 tag !!")
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
