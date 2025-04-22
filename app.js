c8498908ab0181a83f3f4f1d203ba33cbadf1a4c
suyashverma15 <140881601+suyashverma15@users.noreply.github.com>
2025-04-23 00:04:48 +0530

    modified

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const heading = React.createElement(
-    "h1",
-    {id: "heading"},
-    "Hello World from react!!");
+// const heading = React.createElement(
+//     "h1",
+//     {id: "heading"},
+//     "Hello World from react!!");
 
-// nested element is created     
+// nested element is created 
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    [React.createElement(
+        "div",
+        {id: "child"},
+        [React.createElement("h1",{},"I am a h1 tag !!"),
+         React.createElement("h2",{},"I am a h2 tag !!")   
+        ])],
+    [React.createElement(
+        "div",
+        {id: "child2"},
+        [React.createElement("h1",{},"I am a h1 tag !!"),
+         React.createElement("h2",{},"I am a h2 tag !!")   
+        ])]    
+);
+
+// reduncing the code complexcity we use later jsx(JavaScript XML)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading)
+root.render(parent)
